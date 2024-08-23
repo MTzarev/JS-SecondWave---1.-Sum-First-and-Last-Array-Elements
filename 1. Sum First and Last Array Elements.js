b06e5c93acc6154131066d6f8424198b68a6d4d8
Miroslav Tsarev <mtzarev@gmail.com>
2024-08-23 22:53:31 +0300

    Add files via upload

---
+++ 1. Sum First and Last Array Elements.js
@@ -0,0 +1,7 @@
+function sumFirstAndLastArrayElements(arr) {
+let first = arr.shift();
+let last = arr[arr.length-1];
+    console.log(first+last);
+    
+}
+sumFirstAndLastArrayElements([11, 58, 69]);
